5c80afd09486bc6e8c7b23a3869484011a7c94ee
Arindita Pal <arinditapal2003@gmail.com>
2023-09-29 17:01:12 +0530

    response status line sent

--- app/main.py
+++ app/main.py
@@ -2,9 +2,17 @@ import socket
 
 
 def main():
-    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
-    server_socket.accept()  # wait for client
 
+    server_socket = socket.create_server(("localhost", 4221), reuse_port=True)
+
+    conn, addr = server_socket.accept()
+
+    print(conn, addr) 
+    with conn:
+        data = conn.recv(1024)
+        conn.send(b'HTTP/1.1 200 OK\r\n\r\n')
+
+    print("connection ended")
 
 if __name__ == "__main__":
     main()
