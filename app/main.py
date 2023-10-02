c57f0270bb79af5e651444554ede623fda96967e
Arindita Pal <arinditapal2003@gmail.com>
2023-10-02 15:13:08 +0530

    parsed header

--- app/main.py
+++ app/main.py
@@ -10,15 +10,36 @@ def main():
 
         conn, addr = server_socket.accept()
         data = conn.recv(1024).decode('utf-8')
-        with conn:
-            request_first_line = data.split('\r\n')[0]
-            request_path = request_first_line.split()[1]
 
-            print("this is the requrest path:", request_path)
+        parsed_data = data.strip().split("\r\n")
+        status_line = parsed_data[0]
+        parsed_header = parsed_data[1:]
+
+        request_method = status_line.split()[0]
+        request_path = status_line.split()[1]
+
+        # print(request_method, request_path)
+        # print(parsed_header)
+        headers = {}
+        for line in parsed_header:
+            line = line.split(':')
+            headers[line[0].lower()] = line[1].strip()
+
+        print(f"start line: {status_line} \nheaders: {headers}")
+
+
+        with conn:
 
             if request_path == '/':
                 print("send the root")
                 conn.send(b'HTTP/1.1 200 OK\r\n\r\n')
+
+            elif "/user-agent" in request_path:
+                response_body = headers['user-agent']
+                print("response body: ", response_body)
+                response = f"HTTP/1.1 200 OK\r\nContent-type: text/plain\r\ncontent-length: {len(response_body)}\r\n\r\n{response_body}"
+
+                conn.send(response.encode('utf-8'))
 
             elif "/echo/" in request_path:
                 print("req has mess")
@@ -30,6 +51,7 @@ def main():
 
                 print(message)
                 conn.send(res_body.encode('utf-8'))
+
             else:
                 conn.send(b'HTTP/1.1 404 NOT FOUND\r\n\r\n')
 
---
+++ app/trial.py
@@ -0,0 +1,11 @@
+s_pair = "name: harry potter"
+
+s_pair = s_pair.split(':')
+
+print(s_pair)
+
+names = {}
+
+names[s_pair[0]] = s_pair[1]
+
+print(names)
